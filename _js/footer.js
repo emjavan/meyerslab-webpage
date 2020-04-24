561184b613cb3a41d46236e9e8c289baa0855c32
Emily Javan <emjavan@gmail.com>
2020-04-24 03:07:22 -0500

    updated github in footer

--- _js/footer.js
+++ _js/footer.js
@@ -7,7 +7,7 @@ document.write(
 '            <!-- <li><a href="http://news.bootswatch.com/" onclick="pageTracker._link(this.href); return false;">Blog</a></li> -->' +
 '            <li><a href="./">Home</a></li>' +
 '            <li><a href="https://twitter.com/meyerslab">Twitter</a></li>' +
-'            <li><a href="https://github.com/meyerslab">GitHub</a></li>' +
+'            <li><a href="https://github.com/MeyersLabUTexas">GitHub</a></li>' +
 '          </ul>' +
 '          <p>Created under the <a href="https://github.com/thomaspark/bootswatch/blob/gh-pages/LICENSE">MIT License</a>.</p>' +
 '          <p>Based on <a href="http://getbootstrap.com/" rel="nofollow">Bootstrap</a> using  <a href="http://bootswatch.com/" rel="nofollow">Bootswatch</a>. Web fonts from <a href="http://www.google.com/webfonts" rel="nofollow">Google</a>.</p>' +
